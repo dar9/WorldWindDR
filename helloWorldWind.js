9e7d77899bc3108e5dec87068454dac7ecdcf2e5
dar9 <65439539+dar9@users.noreply.github.com>
2024-09-24 23:46:27 -0600

    Add files via upload
    also add world wind and the image not sure if it will work

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -6,3 +6,30 @@ wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+
+var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+wwd.addLayer(placemarkLayer);
+
+
+
+var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+
+placemarkAttributes.imageOffset = new WorldWind.Offset(
+    WorldWind.OFFSET_FRACTION, 0.3,
+    WorldWind.OFFSET_FRACTION, 0.0);
+
+placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, 0.5,
+            WorldWind.OFFSET_FRACTION, 1.0);
+
+
+placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "plain-red.png";
+
+var position = new WorldWind.Position(55.0, -106.0, 100.0);
+var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+
+placemark.label = "You are Gay";
+placemark.alwaysOnTop = true;
+
+placemarkLayer.addRenderable(placemark);
